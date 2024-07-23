5946c6616c34de7e3e3febe1219eaf7b26f8777a
NeelManjanath <neelmanjanath@gmail.com>
2024-07-23 15:50:41 +0530

    add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+import { createBrowserRouter } from 'react-router-dom'
+const router =  createBrowserRouter ([
+  {
+    path: "/react-calculator/",
+    element: <App />
+  } 
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ReactProvider router = {router}/>
   </React.StrictMode>,
 )
